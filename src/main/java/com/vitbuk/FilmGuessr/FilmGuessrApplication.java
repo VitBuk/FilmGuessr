80cfe8a404bf48f101df687c9c37a4a531905e16
VitBuk <vitbuk@gmail.com>
2024-09-24 17:16:55 +0300

    setting up project witih Spring Boot Initlzr

---
+++ src/main/java/com/vitbuk/FilmGuessr/FilmGuessrApplication.java
@@ -0,0 +1,13 @@
+package com.vitbuk.FilmGuessr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmGuessrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmGuessrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitbuk/FilmGuessr/FilmGuessrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitbuk.FilmGuessr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmGuessrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
