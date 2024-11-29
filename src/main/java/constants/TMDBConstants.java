e599d9ecc823b3843e478681f26b2aebdaea049c
VitBuk <vitbuk@gmail.com>
2024-11-29 17:13:23 +0200

    gitIgnore update

--- src/main/java/constants/TMDBConstants.java
+++ src/main/java/constants/TMDBConstants.java
@@ -4,4 +4,6 @@ public class TMDBConstants {
         public static final String APIKey ="f966300eca61c309e3ba1dca32383af8";
         public static final String APIKeyName = "api_key";
         public static final String APIVersion = "3";
+        public static final String BaseUrl = "https://api.themoviedb.org/";
+
 }
